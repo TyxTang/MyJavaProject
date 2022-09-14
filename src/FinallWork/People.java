9ee8c3a8003cf326d4e667e6430717153dbdf0d0
TyxTang <2222002219@qq.com>
2022-09-14 17:10:54 +0800

    Finall Work 22.9.14

---
+++ src/FinallWork/People.java
@@ -0,0 +1,28 @@
+package FinallWork;
+
+class People_1{
+    private double height;
+    private double weight;
+    public void setHeight(double height){
+        this.height = height;
+    }
+    public void setWeight(double weight){
+        this.weight = weight;
+    }
+    public void speak(){
+        System.out.println("身高是" + height + " " + "体重是" + weight);
+    }
+}
+
+public class People {
+    public static void main(String[] args){
+        People_1 p = new People_1();
+        p.setHeight(1.78);
+        p.setWeight(62);
+        p.speak();
+    }
+
+
+}
+
+
